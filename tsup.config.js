be3f4955ce0428a4fb6261546c2e4311ca727da2
Dominik Ferber <dominik.ferber@gmail.com>
2022-10-21 09:44:32 +0300

    drop esm

--- tsup.config.js
+++ tsup.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
+  format: ['cjs'],
   splitting: true,
   sourcemap: false,
   minify: true,

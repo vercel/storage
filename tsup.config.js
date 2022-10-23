888b861283d72c2c593a4b165105677b01c474a2
Dominik Ferber <dominik.ferber@gmail.com>
2022-10-23 18:45:59 +0300

    drop cjs support, fix dynamic import in esm (#32)
    * add webpackIgnore: true comment to dynamic import
    
    * drop cjs support

--- tsup.config.js
+++ tsup.config.js
@@ -1,9 +1,10 @@
+import fs from 'node:fs';
 import { defineConfig } from 'tsup';
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
+  format: ['esm'],
   splitting: true,
   sourcemap: false,
   minify: true,
@@ -11,4 +12,12 @@ export default defineConfig({
   skipNodeModulesBundle: true,
   dts: true,
   external: ['node_modules'],
+  onSuccess: () => {
+    const path = './dist/index.js';
+    const content = fs.readFileSync(path, 'utf-8');
+    fs.writeFileSync(
+      path,
+      content.replace('import(', 'import(/* webpackIgnore: true */'),
+    );
+  },
 });

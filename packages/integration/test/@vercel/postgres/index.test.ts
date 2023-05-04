a3dbd3754f6e60ea62017d1e4962293dd150bac1
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2023-05-04 12:26:10 -0600

    fix: Dumb date tz stuff

--- packages/integration/test/@vercel/postgres/index.test.ts
+++ packages/integration/test/@vercel/postgres/index.test.ts
@@ -7,7 +7,7 @@ const expectedRows = [
     email: 'rauchg@vercel.com',
     image:
       'https://pbs.twimg.com/profile_images/1576257734810312704/ucxb4lHy_400x400.jpg',
-    created_at: '2023-05-04T01:16:40.854Z',
+    created_at: null,
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ const expectedRows = [
     email: 'lee@vercel.com',
     image:
       'https://pbs.twimg.com/profile_images/1587647097670467584/adWRdqQ6_400x400.jpg',
-    created_at: '2023-05-04T01:16:40.854Z',
+    created_at: null,
   },
   {
     id: 3,
@@ -23,7 +23,7 @@ const expectedRows = [
     email: 'stey@vercel.com',
     image:
       'https://pbs.twimg.com/profile_images/1506792347840888834/dS-r50Je_400x400.jpg',
-    created_at: '2023-05-04T01:16:40.854Z',
+    created_at: null,
   },
 ];
 

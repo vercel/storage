f45a5b6fb4fe14de068b7488f5053a14a47663db
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2023-05-07 16:38:31 -0600

    feat: Allow `unsafeUnescaped` in `sql` template calls

--- packages/postgres/src/sql-template.test.ts
+++ packages/postgres/src/sql-template.test.ts
@@ -1,7 +1,7 @@
 import { VercelPostgresError } from './error';
-import { sqlTemplate } from './sql-template';
+import { sqlTemplate, unsafeUnescaped } from './sql-template';
 
-const cases = [
+const validCases = [
   {
     input: sqlTemplate`SELECT * FROM users WHERE id = ${123}`,
     output: ['SELECT * FROM users WHERE id = $1', [123]],
@@ -24,14 +24,70 @@ const cases = [
 ];
 
 describe('sql', () => {
-  it.each(cases)('should return a query and params', ({ input, output }) => {
-    expect(input).toEqual(output);
-  });
-  it('throws when not used as a tagged literal', () => {
+  it.each(validCases)(
+    'should return a query and params',
+    ({ input, output }) => {
+      expect(input).toEqual(output);
+    },
+  );
+  it('throws when accidentally not used as a tagged literal', () => {
     const likes = 100;
     expect(() => {
       // @ts-expect-error - intentionally incorrect usage
       sqlTemplate(`SELECT * FROM posts WHERE likes > ${likes}`);
     }).toThrow(VercelPostgresError);
   });
+  it('throws when deliberately not used as a tagged literal to try to make us look dumb', () => {
+    const likes = 100;
+    expect(() => {
+      // @ts-expect-error - intentionally incorrect usage
+      sqlTemplate([`SELECT * FROM posts WHERE likes > ${likes}`]);
+    }).toThrow(VercelPostgresError);
+    expect(() => {
+      // @ts-expect-error - intentionally incorrect usage
+      sqlTemplate(`SELECT * FROM posts WHERE likes > ${likes}`, 123);
+    }).toThrow(VercelPostgresError);
+  });
+});
+
+const unescapedCases = [
+  {
+    input: sqlTemplate`SELECT * FROM ${unsafeUnescaped(
+      'users',
+    )} WHERE id = ${123}`,
+    output: ['SELECT * FROM users WHERE id = $1', [123]],
+  },
+  {
+    input: sqlTemplate`SELECT * ${unsafeUnescaped(
+      'FROM',
+    )} users WHERE id = ${123} AND name = ${'John'}`,
+    output: ['SELECT * FROM users WHERE id = $1 AND name = $2', [123, 'John']],
+  },
+  {
+    input: sqlTemplate`SELECT ${unsafeUnescaped(
+      '*',
+    )} FROM users WHERE name = ${'John; DROP TABLE users;--'}`,
+    output: [
+      'SELECT * FROM users WHERE name = $1',
+      ['John; DROP TABLE users;--'],
+    ],
+  },
+  {
+    input: sqlTemplate`SELECT * FROM users WHERE name = ${unsafeUnescaped(
+      "'John'; DROP TABLE users;--",
+    )}`,
+    output: [
+      "SELECT * FROM users WHERE name = 'John'; DROP TABLE users;--",
+      [],
+    ],
+  },
+];
+
+describe('unsafeUnescaped', () => {
+  it.each(unescapedCases)(
+    'should return a query with an unchanged value',
+    ({ input, output }) => {
+      expect(input).toEqual(output);
+    },
+  );
 });
--- packages/postgres/src/sql-template.ts
+++ packages/postgres/src/sql-template.ts
@@ -1,24 +1,93 @@
 import { VercelPostgresError } from './error';
 
-export type Primitive = string | number | boolean | undefined | null;
+export type Primitive =
+  | string
+  | number
+  | boolean
+  | undefined
+  | null
+  | UnsafeUnescaped;
+
+const unsafeUnescapedSecret = Symbol();
 
 export function sqlTemplate(
   strings: TemplateStringsArray,
   ...values: Primitive[]
 ): [string, Primitive[]] {
-  if (!Array.isArray(strings)) {
+  if (!isTemplateStringsArray(strings) || !Array.isArray(values)) {
     throw new VercelPostgresError(
       'incorrect_tagged_template_call',
       "It looks like you tried to call `sql` as a function. Make sure to use it as a tagged template.\n\tExample: sql`SELECT * FROM users`, not sql('SELECT * FROM users')",
     );
   }
 
-  // ts tries to annotate `strings` with `& any[]` because of the prior check which breaks the type
-  let result = (strings as TemplateStringsArray)[0] ?? '';
+  let result = strings[0] ?? '';
+  const params: Exclude<Primitive, UnsafeUnescaped>[] = [];
 
+  let offset = 0;
   for (let i = 1; i < strings.length; i++) {
-    result += `$${i}${(strings as TemplateStringsArray)[i] ?? ''}`;
+    const value = values[i - 1];
+    if (isUnsafeUnescaped(value)) {
+      result += `${value[unsafeUnescapedSecret]}${strings[i] ?? ''}`;
+      offset++;
+      continue;
+    }
+    params.push(value);
+    result += `$${i - offset}${strings[i] ?? ''}`;
   }
 
-  return [result, values];
+  return [result, params];
+}
+
+// We do this so that no possible value can be `UnsafeUnescaped` without running through the
+// `unsafeUnescaped` function. This is important because we don't want to accidentally allow
+// some random input to "pretend" to be `unsafeUnescaped`.
+interface UnsafeUnescaped {
+  [unsafeUnescapedSecret]: string;
+}
+
+/**
+ * During normal template string interpolation, values are replaced with pgsql parameters. For example,
+ * ```ts
+ * sql`SELECT * FROM users WHERE id = ${id}`;
+ * ```
+ * becomes
+ * ```ts
+ * pg.query('SELECT * FROM users WHERE id = $1', [id]);
+ * ```
+ *
+ * Sometimes, it's useful to be able to insert values into a query in places where pgsql parameters aren't allowed.
+ * For example, to parameterize a column or table name. To do this, you can use `unsafeUnescaped`:
+ * ```ts
+ * sql`SELECT * FROM ${unsafeUnescaped('users')} WHERE id = ${id}`;
+ * ```
+ * becomes
+ * ```ts
+ * pg.query('SELECT * FROM users WHERE id = $1', [id]);
+ * ```
+ *
+ * This is _unsafe_ and _will_ lead to SQL injection vulnerabilities if you do not sanitize your input.
+ *
+ * @param value The value to ignore during template string interpolation
+ * @returns A value that will be ignored during {@link sqlTemplate} template string interpolation
+ */
+export function unsafeUnescaped(value: string): UnsafeUnescaped {
+  return { [unsafeUnescapedSecret]: value };
+}
+
+function isTemplateStringsArray(
+  strings: unknown,
+): strings is TemplateStringsArray {
+  return (
+    // @ts-expect-error - I don't know how to convince TS that an array can have a property
+    Array.isArray(strings) && 'raw' in strings && Array.isArray(strings.raw)
+  );
+}
+
+function isUnsafeUnescaped(value: unknown): value is UnsafeUnescaped {
+  return (
+    typeof value === 'object' &&
+    value !== null &&
+    unsafeUnescapedSecret in value
+  );
 }

6b8b7a943eeaa62823748460ad5df987ed2bd60c
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2023-05-07 18:03:42 -0600

    fix: No more `sql` tomfoolery + CI fixes (#125)
    * feat: Allow `unsafeUnescaped` in `sql` template calls
    
    * chore: README
    
    * export the new api
    
    * changeset
    
    * fix: Live tests
    
    * lint
    
    * chore: Hopefully fix publint
    
    * chore: Split `unsafeUnescaped` into its own PR

---
+++ .changeset/brown-dodos-sparkle.md
@@ -0,0 +1,5 @@
+---
+'@vercel/postgres': patch
+---
+
+fix: Make it even harder to call `sql` incorrectly
--- packages/postgres/src/sql-template.ts
+++ packages/postgres/src/sql-template.ts
@@ -6,19 +6,27 @@ export function sqlTemplate(
   strings: TemplateStringsArray,
   ...values: Primitive[]
 ): [string, Primitive[]] {
-  if (!Array.isArray(strings)) {
+  if (!isTemplateStringsArray(strings) || !Array.isArray(values)) {
     throw new VercelPostgresError(
       'incorrect_tagged_template_call',
       "It looks like you tried to call `sql` as a function. Make sure to use it as a tagged template.\n\tExample: sql`SELECT * FROM users`, not sql('SELECT * FROM users')",
     );
   }
 
-  // ts tries to annotate `strings` with `& any[]` because of the prior check which breaks the type
-  let result = (strings as TemplateStringsArray)[0] ?? '';
+  let result = strings[0] ?? '';
 
   for (let i = 1; i < strings.length; i++) {
-    result += `$${i}${(strings as TemplateStringsArray)[i] ?? ''}`;
+    result += `$${i}${strings[i] ?? ''}`;
   }
 
   return [result, values];
+}
+
+function isTemplateStringsArray(
+  strings: unknown,
+): strings is TemplateStringsArray {
+  return (
+    // @ts-expect-error - I don't know how to convince TS that an array can have a property
+    Array.isArray(strings) && 'raw' in strings && Array.isArray(strings.raw)
+  );
 }

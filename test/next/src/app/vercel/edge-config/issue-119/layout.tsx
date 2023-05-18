7acb13f107fc446ba455d63fe7ce294a3c536ca1
Dominik Ferber <dominik.ferber@gmail.com>
2023-05-18 15:10:04 +0300

    debug

--- test/next/src/app/vercel/edge-config/issue-119/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const dynamic = 'force-static';
-
-export default function Layout({
-  children,
-}: {
-  children: JSX.Element;
-}): JSX.Element {
-  return children;
-}
--- test/next/src/app/vercel/edge-config/page.tsx
+++ test/next/src/app/vercel/edge-config/page.tsx
@@ -1,12 +1,3 @@
-import Link from 'next/link';
-
 export default function Page(): JSX.Element {
-  return (
-    <div>
-      <h2>Issues</h2>
-      <div>
-        <Link href="/vercel/edge-config/issue-119">Issue 119</Link>
-      </div>
-    </div>
-  );
+  return <div>Not implemented</div>;
 }

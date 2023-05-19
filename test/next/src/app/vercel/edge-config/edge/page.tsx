b7fdcd0ef35edf78c0b6c205731aa887c6d5ff79
Dominik Ferber <dominik.ferber@gmail.com>
2023-05-19 09:15:13 +0300

    add test overview page

--- test/next/src/app/vercel/edge-config/edge/page.tsx
+++
@@ -1,8 +0,0 @@
-import { get } from '@vercel/edge-config';
-
-export const runtime = 'edge';
-
-export default async function Page(): Promise<JSX.Element> {
-  const ids = await get('issue119ids');
-  return <div>{ids?.toString()}</div>;
-}

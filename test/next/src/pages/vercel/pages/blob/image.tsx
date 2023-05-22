813b9f6a2c613862fe860d96a6ec1f421988857e
Fabio Benedetti <fabio.benedetti@vercel.com>
2023-05-22 13:26:29 +0200

    blob-visual-test-playwright (#149)
    add visual regression tests
    
    Co-authored-by: Vincent Voyer <vincent@codeagain.com>

---
+++ test/next/src/pages/vercel/pages/blob/image.tsx
@@ -0,0 +1,32 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import type { GetServerSideProps } from 'next';
+import * as vercelBlob from '@vercel/blob';
+
+export const getServerSideProps: GetServerSideProps = async (req) => {
+  const prefix = req.query.prefix as string;
+  const imgPath = path.join(process.cwd(), 'images');
+  const imageFile = fs.readFileSync(path.join(imgPath, `g.jpeg`));
+
+  const blob = await vercelBlob.put(`${prefix}/g.jpeg`, imageFile, {
+    access: 'public',
+  });
+
+  return {
+    props: {
+      ...blob,
+      uploadedAt: blob.uploadedAt.toString(),
+    },
+  };
+};
+
+export default function Blob(props: vercelBlob.BlobResult): JSX.Element {
+  return (
+    <div>
+      <h1>Render an upload image on the browser</h1>
+
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img alt="test" src={props.url} />
+    </div>
+  );
+}

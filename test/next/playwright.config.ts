f80e158798142823f272d1a155a8d234dce5b8ee
Vincent Voyer <vincent@codeagain.com>
2023-05-22 12:11:06 +0200

    ok

--- test/next/playwright.config.ts
+++ test/next/playwright.config.ts
@@ -27,6 +27,8 @@ const config: PlaywrightTestConfig = {
   // Artifacts folder where screenshots, videos, and traces are stored.
   outputDir: 'test-results/',
 
+  maxFailures: 1,
+
   // Run your local dev server before starting the tests:
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
   webServer: process.env.PLAYWRIGHT_TEST_BASE_URL ? undefined : webServer,

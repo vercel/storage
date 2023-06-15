6eafd26f0cac70d6402b0821efe8f9d89720e13c
Fabio Benedetti <fabio.benedetti@vercel.com>
2023-06-15 12:47:14 +0200

    fix browser test

--- packages/blob/src/index.browser.test.ts
+++ packages/blob/src/index.browser.test.ts
@@ -1,43 +1,33 @@
-import { type Interceptable, MockAgent, setGlobalDispatcher } from 'undici';
 import { put } from './index';
 
+jest.mock('undici', () => ({
+  fetch: (): unknown =>
+    Promise.resolve({
+      status: 200,
+      json: () =>
+        Promise.resolve({
+          url: `${BASE_URL}/storeid/foo-id.txt`,
+          size: 12345,
+          uploadedAt: '2023-05-04T15:12:07.818Z',
+          pathname: 'foo.txt',
+          contentType: 'text/plain',
+          contentDisposition: 'attachment; filename="foo.txt"',
+        }),
+    }),
+}));
 const BASE_URL = 'https://blob.vercel-storage.com';
-const mockAgent = new MockAgent();
-mockAgent.disableNetConnect();
-
-setGlobalDispatcher(mockAgent);
-
-const mockedFileMeta = {
-  url: `${BASE_URL}/storeid/foo-id.txt`,
-  size: 12345,
-  uploadedAt: '2023-05-04T15:12:07.818Z',
-  pathname: 'foo.txt',
-  contentType: 'text/plain',
-  contentDisposition: 'attachment; filename="foo.txt"',
-};
 
 describe('blob client', () => {
-  let mockClient: Interceptable;
-
   beforeEach(() => {
-    mockClient = mockAgent.get(BASE_URL);
     jest.resetAllMocks();
   });
 
   describe('put', () => {
     beforeEach(() => {
-      mockClient = mockAgent.get(BASE_URL);
       jest.resetAllMocks();
     });
 
     it('should upload a file from the client', async () => {
-      mockClient
-        .intercept({
-          path: () => true,
-          method: 'PUT',
-        })
-        .reply(200, mockedFileMeta);
-
       await expect(
         put('foo.txt', 'Test Body', {
           access: 'public',

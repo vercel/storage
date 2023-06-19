69cc3cb2ee20abf55487d5319454acbfcfc71cff
Vincent Voyer <vincent@codeagain.com>
2023-06-19 13:11:46 +0200

    force dynamic

--- test/next/src/app/api/vercel/postgres-kysely/app/edge/route.ts
+++ test/next/src/app/api/vercel/postgres-kysely/app/edge/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 import { queryUsers } from '@/lib/postgres-kysely';
 
 export const runtime = 'edge';
+export const dynamic = 'force-dynamic';
 
 export async function GET(): Promise<Response> {
   try {

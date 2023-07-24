a82b69e7162c8e7cd0e8a8bea060a89097ff4cf5
Fabio Benedetti <fabio.benedetti@vercel.com>
2023-07-24 12:31:11 +0200

    skip client upload authentication locally (#298)
    * skip client upload authentication locally
    
    * move local validation check

--- test/next/src/app/vercel/blob/validate-upload-token.ts
+++ test/next/src/app/vercel/blob/validate-upload-token.ts
@@ -3,6 +3,7 @@ import type { IncomingMessage } from 'node:http';
 export function validateUploadToken(
   request: IncomingMessage | Request,
 ): boolean {
+  if (process.env.NODE_ENV === 'development') return true;
   const cookie =
     'credentials' in request
       ? request.headers.get('cookie') ?? ''

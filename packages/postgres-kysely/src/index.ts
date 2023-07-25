ac9f7cd57f503ed952a35ad23018dba7925b2d5d
Marcus Hann <marcus@keplerit.uk>
2023-07-25 21:52:25 +0100

    Add postgresql adapter

--- packages/postgres-kysely/src/index.ts
+++ packages/postgres-kysely/src/index.ts
@@ -1,17 +1,60 @@
-import type { DatabaseConnection, Dialect, Driver } from 'kysely';
-import { PostgresDialect, PostgresDriver, Kysely } from 'kysely';
+import type {
+  DatabaseConnection,
+  Dialect,
+  DialectAdapter,
+  DialectAdapterBase,
+  Driver,
+  MigrationLockOptions,
+} from 'kysely';
+import {
+  PostgresDialect,
+  PostgresDriver,
+  Kysely,
+  PostgresAdapter,
+  sql,
+} from 'kysely';
 import type { Pool } from '@neondatabase/serverless';
 import { createPool } from '@vercel/postgres';
 import type { VercelPostgresPoolConfig } from '@vercel/postgres';
 import { VercelPostgresKyselyError } from './error';
 
+// Random id for our transaction lock.
+const LOCK_ID = BigInt('3853314791062309107');
+
 type VercelPostgresDialectConfig = VercelPostgresPoolConfig & {
   pool: Pool;
 };
 
+class VercelPostgresAdapter
+  extends PostgresAdapter
+  implements DialectAdapterBase
+{
+  get supportsTransactionalDdl(): boolean {
+    return false;
+  }
+
+  async acquireMigrationLock(
+    db: Kysely<any>,
+    _opt: MigrationLockOptions,
+  ): Promise<void> {
+    await sql`select pg_advisory_lock(${sql.lit(LOCK_ID)})`.execute(db);
+  }
+
+  async releaseMigrationLock(
+    db: Kysely<any>,
+    _opt: MigrationLockOptions,
+  ): Promise<void> {
+    await sql`select pg_advisory_unlock(${sql.lit(LOCK_ID)})`.execute(db);
+  }
+}
+
 class VercelPostgresDialect extends PostgresDialect implements Dialect {
   constructor(private config: VercelPostgresDialectConfig) {
     super(config);
+  }
+
+  createAdapter(): DialectAdapter {
+    return new VercelPostgresAdapter();
   }
 
   createDriver(): Driver {

7ad3a84d2bf0e4ce59d9ba55d6ab6d8fefab027d
Andy Bitz <artzbitz@gmail.com>
2023-12-08 23:36:03 +0100

    [edge-config] Add tracing through @openetelemetry/api

--- packages/edge-config/src/utils/index.ts
+++ packages/edge-config/src/utils/index.ts
@@ -1,4 +1,5 @@
 import type { Connection } from '../types';
+import { trace } from './tracing';
 
 export const ERRORS = {
   UNEXPECTED: '@vercel/edge-config: Unexpected error',
@@ -42,14 +43,17 @@ export function assertIsKeys(keys: unknown): asserts keys is string[] {
 /**
  * Creates a deep clone of an object.
  */
-export function clone<T>(value: T): T {
-  // only available since node v17.0.0
-  if (typeof structuredClone === 'function') return structuredClone<T>(value);
+export const clone = trace(
+  function clone<T>(value: T): T {
+    // only available since node v17.0.0
+    if (typeof structuredClone === 'function') return structuredClone<T>(value);
 
-  // poor man's polyfill for structuredClone
-  if (value === undefined) return value;
-  return JSON.parse(JSON.stringify(value)) as T;
-}
+    // poor man's polyfill for structuredClone
+    if (value === undefined) return value;
+    return JSON.parse(JSON.stringify(value)) as T;
+  },
+  { name: 'clone' },
+);
 
 /**
  * Parses internal edge config connection strings

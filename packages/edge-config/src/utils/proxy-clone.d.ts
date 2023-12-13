1fa1eee355ed10d2db1ad0050c1b8e8f1a67901a
Dominik Ferber <dominik.ferber@gmail.com>
2023-12-13 20:51:43 +0200

    remove unused file

--- packages/edge-config/src/utils/proxy-clone.d.ts
+++
@@ -1,5 +0,0 @@
-// adds types to the untyped proxy-clone npm package
-declare module 'proxy-clone' {
-  // eslint-disable-next-line import/no-default-export -- k
-  export default function clone<T>(x: T): T;
-}

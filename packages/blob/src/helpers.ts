1611b10c3a25f29f61eaf47c23161b3d2172c09a
Luis Meyer <luis.meyer@vercel.com>
2024-01-15 13:05:19 +0100

    feat(blob): allow inline content disposition for certain blobs

--- packages/blob/src/helpers.ts
+++ packages/blob/src/helpers.ts
@@ -147,7 +147,7 @@ export async function validateBlobApiResponse(
 // This version is used to ensure that the client and server are compatible
 // The server (Vercel Blob API) uses this information to change its behavior like the
 // response format
-const BLOB_API_VERSION = 5;
+const BLOB_API_VERSION = 6;
 
 export function getApiVersionHeader(): { 'x-api-version'?: string } {
   let versionOverride = null;

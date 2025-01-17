41dbc061954793befd4415192a709fe3b3705325
Vincent Voyer <vincent@codeagain.com>
2025-01-17 14:24:23 +0100

    revert

--- packages/blob/src/multipart/upload.ts
+++ packages/blob/src/multipart/upload.ts
@@ -1,4 +1,4 @@
-import bytes from 'pretty-bytes';
+import bytes from 'bytes';
 import throttle from 'throttleit';
 import { BlobServiceNotAvailable, requestApi } from '../api';
 import { debug } from '../debug';

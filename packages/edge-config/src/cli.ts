d80061a1e954da3848f838702b4c1f5f72e5dfab
Dominik Ferber <dominik.ferber@gmail.com>
2025-08-03 19:08:32 +0300

    imporve cli

--- packages/edge-config/src/cli.ts
+++ packages/edge-config/src/cli.ts
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
+/*
+ * Reads all connected Edge Configs and emits them to /tmp/edge-config/$id.json
+ *
+ * Attaches the updatedAt timestamp from the header to the emitted file, since
+ * the endpoint does not currently include it in the response body.
+ */
+
 import { writeFile, mkdir } from 'node:fs/promises';
 import type { Connection, EmbeddedEdgeConfig } from './types';
 import { parseConnectionString } from './utils';
@@ -23,17 +30,24 @@ async function main(): Promise<void> {
 
   await Promise.all(
     connections.map(async (connection) => {
-      const data = await fetch(connection.baseUrl, {
+      const { data, updatedAt } = await fetch(connection.baseUrl, {
         headers: {
           authorization: `Bearer ${connection.token}`,
           // consistentRead
           'x-edge-config-min-updated-at': `${Number.MAX_SAFE_INTEGER}`,
         },
-      }).then((res) => res.json() as Promise<EmbeddedEdgeConfig>);
+      }).then(async (res) => {
+        const ts = res.headers.get('x-edge-config-updated-at');
+
+        return {
+          data: (await res.json()) as EmbeddedEdgeConfig,
+          updatedAt: ts ? Number(ts) : undefined,
+        };
+      });
 
       await writeFile(
         `/tmp/edge-config/${connection.id}.json`,
-        JSON.stringify(data),
+        JSON.stringify({ ...data, updatedAt }),
       );
     }),
   );

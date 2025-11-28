f97e0f4ebf7e2505fc10f47b2a8893f3580a05cc
Dominik Ferber <dominik.ferber@gmail.com>
2025-11-28 18:07:41 +0200

    compare to 1

--- packages/edge-config/src/create-create-client.ts
+++ packages/edge-config/src/create-create-client.ts
@@ -97,7 +97,7 @@ export function createCreateClient({
         process.env.EDGE_CONFIG_DISABLE_DEVELOPMENT_SWR !== '1';
 
       const buildEmbeddedEdgeConfigPromise = (() => {
-        if (process.env.EDGE_CONFIG_SKIP_BUILD_EMBEDDING) return null;
+        if (process.env.EDGE_CONFIG_SKIP_BUILD_EMBEDDING === '1') return null;
         if (!connection || connection.type !== 'vercel') return null;
         return getBuildEmbeddedEdgeConfig(connection.id, fetchCache);
       })();
